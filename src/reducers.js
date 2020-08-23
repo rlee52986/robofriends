3fe86968f9d169380aae8cb1a35857c7c42e6c85
Rocky <rocky@Johns-iMac.local>
2020-08-23 14:44:18 -0500

    removed error warnings from cardlist

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,4 @@
-import { CHANGE_SEARCH_FIELD } = from './constants.js';
+import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 const initialState = {
     searchField: ''
@@ -6,8 +6,8 @@ const initialState = {
 
 export const searchRobots = (state=initialState, action={}) => {
     switch(action.type) {
-        case: CHANGE_SEARCH_FIELD: 
-            return Object.assign({}, state, {searchField: action.payload});
+        case CHANGE_SEARCH_FIELD: 
+            return Object.assign({}, state, { searchField: action.payload });
         default: 
             return state;
     }
